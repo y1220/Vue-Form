c0591695f91a178958755050a915f92ec10cc21d
y1220 <ga.2.y1220@gmail.com>
2024-02-18 18:19:23 +0100

    feat(Twitter): add simple list

---
+++ twitter/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Twitter</h1>
+  <ul>
+    <li>Tweet1</li>
+    <li>Tweet2</li>
+    <li>Tweet3</li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-top: 1rem;
+}
+</style>
