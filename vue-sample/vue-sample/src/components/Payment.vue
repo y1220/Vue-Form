becdcd06db61f346d2a22faac3526386ecbb7333
y1220 <ga.2.y1220@gmail.com>
2024-02-18 17:41:59 +0100

    feat(PaymentList): add log to track elements of list

--- vue-sample/vue-sample/src/components/Payment.vue
+++ vue-sample/vue-sample/src/components/Payment.vue
@@ -1,20 +1,45 @@
 <script setup lang="ts">
 
+const itemName1 = 'pizza';
+const itemPrice1 = '5 euro';
+const itemLink1 = 'https://vitejs.dev';
+
+const itemName2 = 'cola';
+const itemPrice2 = '2 euro';
+const itemLink2 = 'https://vitejs.dev';
+
+const buy = (itemName: string) => {
+  console.log('buy');
+    alert('buy ' + itemName + ' success!');
+}
+
+const input = (event: any) => {
+  console.log('input' + event.target.value);
+    // alert('input: ' + event.target.value);
+}
 </script>
 
 <template>
   <div class="container">
     <h1>Recent payment</h1>
     <div class="payment">
-        <label>pizza</label>
-        <label>5 euro</label>
+        <label>{{ itemName1 }}</label>
+        <label>{{ itemPrice1 }}</label>
+        <a v-bind:href=itemLink1>
+          <img src="/pizza.jpg" class="product-img" alt="Vite logo" /></a>
+          <button v-on:click="buy(itemName1)">Buy</button>
     </div>
     <div class="payment">
-        <label>cola</label>
-        <label>2 euro</label>
+        <label>{{ itemName2 }}</label>
+        <label>{{ itemPrice2 }}</label>
+        <a v-bind:href=itemLink2>
+          <img src="/drink.jpg" class="product-img" alt="Vite logo" /></a>
+          <button v-on:click="buy(itemName2)">Buy</button>
     </div>
+    <input v-on:input="input" />
+
   </div>
-  <label>Payment.js</label>
+  <!-- <label>Payment.js</label> -->
   <br/>
 </template>
 
@@ -37,5 +62,10 @@ label {
     width: 400px;
     background-color: aliceblue;
     margin-bottom: 8px;
+}
+
+.product-img {
+    height: 80px;
+    width: 80px;
 }
 </style>
